a47e7312fc763d3686668166346e494aaf26b1b4
Credrido <156992589+Credrido@users.noreply.github.com>
2024-04-04 10:35:58 +0300

    Add files via upload

---
+++ Key.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Key
+    {
+        public event Action<string> KeyPress;
+
+        public string Name { get; set; }
+
+        public Key(string name)
+        {
+            Name = name;
+        }
+
+        public void Press()
+        {
+            KeyPress?.Invoke(Name);
+        }
+    }
+    public class Computer
+    {
+        public void ReactToPressedKey(string name)
+        {
+            Console.WriteLine($" Это не та клавиша");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Event();
+        }
+
+        static void Event()
+        {
+            Key key = new Key("1");
+
+            Computer c = new Computer();
+
+            key.KeyPress += c.ReactToPressedKey;
+
+            int counter = 10;
+
+            while(Console.ReadKey().Key != ConsoleKey.O)
+            {
+                key.Press();
+                if(counter-1 >= 5 )
+                {
+                    Console.WriteLine($"У тебя осталось {counter - 1} попыток");
+                }
+                if ((counter < 5) && (counter >2))
+                {
+                    Console.WriteLine($"У тебя осталось {counter - 1} попытки");
+                }
+                if (counter-1 == 1)
+                {
+                    Console.WriteLine($"У тебя осталось {counter - 1} попытка");
+                }
+
+                Console.WriteLine();
+
+                counter--;
+
+                if(counter == 0)
+                {
+                    Console.WriteLine("У тебя кончились попытки. Проиграл");
+
+                    break;
+                }
+            }
+            if(counter != 0)
+            {
+                Console.WriteLine(" правильная клавиша. Угадал");
+            }
+        }
+    }
+}
